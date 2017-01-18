95909e6fb93010d903a1ff1bde7b32859ba7a94d
Michael Goff <msgoff@users.noreply.github.com>
2017-01-18 12:38:48 -0500

    Delete cards.py

--- cards.py
+++
@@ -1,5 +0,0 @@
-def CardsAndPero(S):
-    hand=re.split('(\D\d{2})',S)[1::2]
-    if len(hand)!=len(set(hand)):
-        return [-1]*4
-    return [13-re.split('(\D)',S).count(x) for x in ['P','K','H','T']]
