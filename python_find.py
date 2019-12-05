2469a2565a716a319170c153d0ddce9388eb36e5
Michael Goff <msgoff@users.noreply.github.com>
2019-12-04 18:37:16 -0800

    Update python_find.py

--- python_find.py
+++ python_find.py
@@ -1,6 +1,24 @@
-#https://stackoverflow.com/questions/2909975/python-list-directory-subdirectory-and-files
-paths = []
-for path, subdirs, files in os.walk('.'):
-    for name in files:
-        resp = os.path.join(path, name)
-        paths.append(resp)
+def get_file_paths(start_pos='.'):
+    #https://stackoverflow.com/questions/2909975/python-list-directory-subdirectory-and-files
+    import os
+    paths = []
+    for path, subdirs, files in os.walk(start_pos):
+        for name in files:
+            resp = os.path.join(path, name)
+            paths.append(resp)
+    return paths
+
+def get_file_checksums(file_name):
+    try:
+        import hashlib
+        f = open(file_name,'rb')
+        data = f.read()
+        f.close()
+        m = hashlib.sha512()
+        m.update(data)
+        return {file_name,m.hexdigest()}
+    except Exception as e:
+        print(e)
+        
+file_paths = get_file_paths()    
+get_file_checksums(file_paths[0])
