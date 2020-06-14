20fdcf33040c127bab389f408572bf44c503b772
Michael Goff <msgoff@users.noreply.github.com>
2020-06-14 17:29:20 +0000

    Create hn_productivity_tools.py

---
+++ hn_productivity_tools.py
@@ -0,0 +1,8 @@
+import requests
+ix = 23468193
+dct = {}
+data=requests.get('https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(ix)).json()
+dct[ix] = data
+for ix in dct[ix]['kids']:
+    data=requests.get('https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(ix)).json()
+    dct[ix] = data
