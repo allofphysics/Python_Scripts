2866487477ae1b1dfc3fadf620b8252273d55ee3
mike <mike@lcd-outlet.com>
2020-07-04 16:08:27 +0000

    cleanup old files

--- system_cmd.py
+++ system_cmd.py
@@ -1,7 +1,6 @@
 def run_command(command):
     import subprocess
     import shlex
-
     lst = []
     counter = 0
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
@@ -9,10 +8,8 @@ def run_command(command):
         output = process.stdout.readline()
         if output == '' and process.poll() is not None:
             return lst
-
         if output:
             lst.append(output.strip())
             counter += 1
-
     rc = process.poll()
     return rc
---
+++ text_highlighting.py
@@ -0,0 +1,4 @@
+#!pip install termcolor
+import re
+from termcolor import colored,cprint
+print(colored('hello', 'red'), colored('world', 'green'))
